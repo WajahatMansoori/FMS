94fdcd8f75c4f0960467e37c687879c348879a6c
muhammad.farooq <muhammad.farooq@DESKTOP-9OIRCA4>
2025-12-05 17:35:05 +0500

    facility service added

---
+++ FacilityManagement.Application/Services/FacilityService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using FacilityManagement.Application.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Shared.Base;
+using Shared.FacilityManagement;
+using Shared.UnitOfWork;
+
+namespace FacilityManagement.Application.Services
+{
+    public class FacilityService : BaseDatabaseService<AstrikFacilityContext>, IFacilityService
+    {
+        private readonly IMapper _mapper;
+        private readonly IFacilityManagementUnitOfWork _facilityManagementUnitOfWork;
+
+        public FacilityService(IMapper mapper ,AstrikFacilityContext context, IFacilityManagementUnitOfWork facilityManagementUnitOfWork, IConfiguration configuration)
+            : base(context)
+        {
+            _mapper = mapper;
+            _context = context;
+            _facilityManagementUnitOfWork = facilityManagementUnitOfWork;
+
+        }    
+
+
+
+
+    }
+}
